2118798007e5947ae58134cd72ea89b881b640e1
Ansh <ansharora19102006@gmail.com>
2026-02-18 22:58:28 +0530

    ifelse practice

---
+++ switch.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Take the day and print the corresponding day
+for ! print Monday,
+for 2 print Tuesday and so on 7 print Sunday
+*/
+
+int main () {
+
+
+
+    return 0;
+}
