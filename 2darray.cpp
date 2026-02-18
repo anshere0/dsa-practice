19407075efdd8f04b314a050172f694107516ed2
Ansh <ansharora19102006@gmail.com>
2026-02-18 23:42:56 +0530

    2d array basic

---
+++ 2darray.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    // 2d array
+    int arr[3][5];
+    // 3 is the row number and 5 is the column number\
+
+
+    arr[1][2] = 78;
+    cout << arr[1][2];
+
+    return 0;
+}
