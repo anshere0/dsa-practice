1494b96a7701713d43a9476678ffcb6a41f21974
Ansh <ansharora19102006@gmail.com>
2026-02-19 00:44:55 +0530

    refrence with array1

--- practice.cpp
+++ practice.cpp
@@ -40,16 +40,22 @@ int main () {
 // changes are made to the original value
 // by attaching an "&" to the parameter, we can pass by reference
 
-void doSomething(string s) {
-    s[0]= 't';
-    cout << s << endl;
-
-
+void doSomething (int arr[], int n) {
+    arr[0]=100;
+    cout<< "value inside function" << arr[0] << endl;
 }
 
+
 int main () {
-    string s= "rohit";
-    doSomething(s);
-    cout << s << endl;
+    int n= 5;
+    int arr[n];
+    for (int i =0; i<n; i=i+1) {
+        cin>> arr[i];
+
+    }
+    doSomething(arr, n);
+
+    cout << "value inside int main" << arr[0] << endl;
+
     return 0;
 }
