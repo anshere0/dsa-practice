80f6a4e5ed44ca5108da51906713c654f9b705e9
Ansh <ansharora19102006@gmail.com>
2026-02-18 23:57:03 +0530

    for loop practice

---
+++ forloops.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+// here, we intiliase the value for i at 1 and then the seecond is the limit till which i will repeat
+// and the third is the increment or the decrement part otheriwse the loop will run infinitely 
+
+    int i;
+    for ( i=1; i<=25; i=i+5) {
+        cout << "ansh is learning " <<  i << endl;
+
+    }
+    cout <<  i <<endl;
+
+return 0;
+}
