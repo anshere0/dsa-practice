d839155c69b17eef34e80c43ba5d9978d3b93dd6
Ansh <ansharora19102006@gmail.com>
2026-02-19 00:04:57 +0530

    while loop practice

---
+++ while.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    while(i<=5) {
+
+        cout << "ansh is learning " << i << endl;
+        i = i+1;
+
+
+    }
+    do {
+        
+        cout << "ansh is learning " << i << endl;
+        i = i+1;
+    }  while (i<=5);
+
+
+
+
+    return 0;
+}
